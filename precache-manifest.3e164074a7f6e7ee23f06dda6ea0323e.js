6ac894e115cb3cf818d3ed5e0a882049a286fd80
Shripal <shripal@gmail.com>
2023-06-03 14:33:29 +0530

    Updates

---
+++ precache-manifest.3e164074a7f6e7ee23f06dda6ea0323e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5ccb006403f1ab55d9",
+    "url": "/News-App/static/css/main.ab70c6ae.chunk.css"
+  },
+  {
+    "revision": "4e5ccb006403f1ab55d9",
+    "url": "/News-App/static/js/main.4e5ccb00.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/News-App/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "14c227b7df9ab223e93a",
+    "url": "/News-App/static/js/2.14c227b7.chunk.js"
+  },
+  {
+    "revision": "09dd51649c4c5b6f56e2",
+    "url": "/News-App/static/js/runtime~main.09dd5164.js"
+  },
+  {
+    "revision": "717659f345c9a3024f85b07a488ba8be",
+    "url": "/News-App/index.html"
+  }
+];
